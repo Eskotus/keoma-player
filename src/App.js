db89ae1a8583539134a1774d82776d07219c1de1
tsimoli <turo.simolin@gmail.com>
2017-04-01 21:46:36 +0300

    Initializing peak.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Peaks from '../node_modules/peaks.js/peaks.js';
 class App extends Component {
-  _peaksDiv: any;
-  _audio: any;
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    const p = Peaks.init({
+      container: this._peaksDiv,
+      mediaElement: this._audio,
+      dataUri: {
+        arrayBuffer: '/assets/sample.dat',
+        json: 'assets/sample.json'
+      },
+      logger: console.error.bind(console)
+    });
+  }
   render() {
     return (
-      <div className="App" ref={(elem) => {this._peaksDiv = elem}}>
-        <audio ref={(elem) => {this._audio = elem}}>
-          <source src="assets/sample.mp3" type="audio/mpeg" />
-          <source src="assets/sample.ogg" type="audio/ogg" />
-          Your browser does not support the audio tag.
-        </audio>
+      <div className="App" ref={(elem) => {this._peaksDiv = elem;}}>
+          <audio ref={(elem) => {this._audio = elem;}}>
+            <source src="assets/sample.mp3" type="audio/mpeg" />
+            <source src="assets/sample.ogg" type="audio/ogg" />
+            Your browser does not support the audio tag.
+          </audio>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Peaks from 'peaks.js';
 import App from './App';
 import './index.css';
-
-const p = Peaks.init({
-  container: App._peaksDiv,
-  mediaElement: App._audio,
-  dataUri: {
-    arrayBuffer: 'assets/sample.dat'
-  },
-  logger: console.error.bind(console),
-  
-});
 
 ReactDOM.render(
   <App />,
