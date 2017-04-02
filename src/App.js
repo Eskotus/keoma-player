ad79bcf361ff5f2950baf262d1013733a35eb824
Eero Saikku <esaikku@gmail.com>
2017-04-03 01:28:06 +0300

    Working buttons

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Peaks from '../node_modules/peaks.js/peaks.js';
+
+type Props = { prop: string };
+
 class App extends Component {
-  constructor(props) {
+  _peaksDiv: HTMLDivElement;
+  _audio: HTMLAudioElement;
+  _zoomIn: HTMLButtonElement;
+  _zoomOut: HTMLButtonElement;
+  _addSegment: HTMLButtonElement;
+  _addPoint: HTMLButtonElement;
+  _logData: HTMLButtonElement;
+
+  constructor(props: Props) {
     super(props);
   }
   componentDidMount() {
-    const p = Peaks.init({
+    var p = Peaks.init({
       container: this._peaksDiv,
       mediaElement: this._audio,
       dataUri: {
@@ -18,15 +29,48 @@ class App extends Component {
       },
       logger: console.error.bind(console)
     });
+    this._zoomIn.onclick = p.zoom.zoomIn.bind(p);
+    this._zoomOut.onclick = function () {
+      p.zoom.zoomOut();
+    }
+
+    this._addSegment.onclick = function () {
+      p.segments.add({
+        startTime: p.time.getCurrentTime(),
+        endTime: p.time.getCurrentTime() + 10,
+        editable: true
+      });
+    };
+
+    this._addPoint.onclick = function() {
+      p.points.add({
+        timestamp: p.time.getCurrentTime(),
+        editable: true
+      });
+    };
+
+    this._logData.onclick = function() {
+      console.log('Segments', p.segments.getSegments());
+      console.log('Points', p.points.getPoints());
+    };
+
   }
   render() {
     return (
-      <div className="App" ref={(elem) => {this._peaksDiv = elem;}}>
-          <audio ref={(elem) => {this._audio = elem;}}>
+      <div className="App">
+        <div id="first-waveform-visualiser-container" ref={(elem) => {this._peaksDiv = elem;}} />
+        <div id="demo-controls">
+          <audio controls="controls" ref={(elem) => {this._audio = elem;}}>
             <source src="assets/sample.mp3" type="audio/mpeg" />
             <source src="assets/sample.ogg" type="audio/ogg" />
             Your browser does not support the audio tag.
           </audio>
+          <button ref={(elem) => {this._zoomIn = elem;}}>Zoom in</button>
+          <button ref={(elem) => {this._zoomOut = elem;}}>Zoom out</button>
+          <button ref={(elem) => {this._addSegment = elem;}} data-action="add-segment">Add a Segment at current time</button>
+          <button ref={(elem) => {this._addPoint = elem;}} data-action="add-point">Add a Point at current time</button>
+          <button ref={(elem) => {this._logData = elem;}} data-action="log-data">Log segments/points</button>
+        </div>
       </div>
     );
   }
